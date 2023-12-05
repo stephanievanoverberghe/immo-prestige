e24f81343ca308c4462de2ac46655bc568ac29a2
Stéphanie Vanoverberghe <orangestreet@live.fr>
2023-12-05 21:22:34 +0100

    video

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,1 @@
 document.getElementById("banner-video").playbackRate = 0.5;
-
-document.getElementById('banner-video').addEventListener('loadeddata', function () {
-    document.getElementById('banner-video-img').style.display = 'none';
-});

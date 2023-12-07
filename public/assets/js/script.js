c7a85888c002142cc244dd3216dd3ba110b3cc91
Stéphanie Vanoverberghe <orangestreet@live.fr>
2023-12-07 19:34:19 +0100

    js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,7 +3,6 @@ document.getElementById("banner-video").playbackRate = 0.5;
 document.querySelectorAll('.dropdown-hover').forEach(dropdown => {
     dropdown.addEventListener('mouseover', function () {
         this.querySelector('.dropdown-menu').classList.add('show');
-        console.log('dropdown-hover');
     });
 
     dropdown.addEventListener('mouseout', function () {
